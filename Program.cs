05d15ff410043fd6a4e1d3c6dbff534ff0568f65
Rimas Misevičius <rmisev@users.noreply.github.com>
2016-10-01 01:25:33 +0300

    Set the user interface to display in the same culture as that set in Control Panel

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ namespace CheckSum
         [STAThread]
         static void Main()
         {
+            // https://msdn.microsoft.com/en-us/library/system.threading.thread.currentuiculture(v=vs.110).aspx
+            // Set the user interface to display in the same culture as that set in Control Panel.
+            System.Threading.Thread.CurrentThread.CurrentUICulture = System.Threading.Thread.CurrentThread.CurrentCulture;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormSum());
